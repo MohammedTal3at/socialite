e1028f299a62f6d5e7e00df15228c52b4fdeb846
Mohamed Talaat <m.talaat377@gmail.com>
2018-05-16 12:50:27 +0200

    add facebook login

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//facebook login
+Route::get('login/facebook', 'Auth\LoginController@redirectToProvider')->name('login.facebook');
+Route::get('login/facebook/callback', 'Auth\LoginController@handleProviderCallback');
